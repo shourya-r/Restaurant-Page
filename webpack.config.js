e70ffa1010aeff3d4eaff97bb31908daebbebdcc
shourya-r <rajputshourya7@gmail.com>
2024-03-24 12:04:21 +0530

    Set up the skeleton project

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry : './src/index.js',   
+    output : {
+        filename : 'main.js',
+        path : path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html', 
+        })
+    ],
+};
